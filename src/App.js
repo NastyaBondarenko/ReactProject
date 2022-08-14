015af98bc11631b48653307ce01e94004c32c1d3
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-08-14 22:05:43 +0300

    43

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Dialogs from './components/Dialogs/Dialogs';
 // import {BrowserRouter, Route} from "react-router-dom";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
 
     return (
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path="/profile" element = {<Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />}/>
-                        <Route path="/dialogs/*" element = {<Dialogs store={props.store}/>}/>
-                                                                     {/*// messages={props.state.dialogsPage.messages}/>}/>*/}
-                    </Routes>
-                </div>
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path="/profile" element={<Profile store={props.store}/>}/>
+                    <Route path="/dialogs/*" element={<DialogsContainer store={props.store}/>}/>
+                    {/*// messages={props.state.dialogsPage.messages}/>}/>*/}
+                </Routes>
             </div>
-);
+        </div>
+    );
 }
 
 export default App;
