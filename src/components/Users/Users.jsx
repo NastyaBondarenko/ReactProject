142c8001ec5db190e0445a4c9ede7e9bce3f68c9
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-08-17 02:10:55 +0300

    54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,14 @@ import * as axios from "axios";
 import userPhoto from '../../assets/pngtree-user-vector-avatar-png-image_4830521.jpg';
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        alert('NEW')
+
+    componentDidMount() {
 
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
 
             this.props.setUsers(response.data.items);
         })
     }
-
 
     render() {
         return <div>
