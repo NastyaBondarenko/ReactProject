ab73b5a940b9e2bc3bfd47e7333a07ccf9c07377
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-08-23 23:59:31 +0300

    63

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import * as axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "dcf9f632-9c8c-4b17-aca6-4e954b44d3a9"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            });
+    }
+}
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data
+        });
+}
