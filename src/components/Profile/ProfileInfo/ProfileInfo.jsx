7a7924d5400c36028281b5b5498a9e80917d2e52
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-08-27 21:36:34 +0300

    71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if(!props.profile){
@@ -8,12 +9,12 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div><img
-                src="https://st.depositphotos.com/1038117/1453/i/600/depositphotos_14534069-stock-photo-perfect-sky-and-water-of.jpg"/>
-            </div>
+            {/*<div><img*/}
+            {/*    src="https://st.depositphotos.com/1038117/1453/i/600/depositphotos_14534069-stock-photo-perfect-sky-and-water-of.jpg"/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava+discr
+               <ProfileStatus status ={"Hello my friends"}/>
             </div>
         </div>
     )
