43444a674e3f74a766745170465cf14906b9867d
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-09-12 10:05:40 +0300

    75-1

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Login=(props)=>{
-    return <h1>LOGIN</h1>
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input placeholder={"Login"}/>
+            </div>
+            <div>
+                <input placeholder={"Password"}/>
+            </div>
+            <div>
+                <input type={"checkbox"}/>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const Login = (props) => {
+    return <div>
+        <h1>Login</h1>
+        <LoginForm/>
+    </div>
 }
 
 export default Login;
